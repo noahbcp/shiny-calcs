6492021e5d260c879d71b6939cfd856b690714a4
noahbcp <noah_piper@hotmail.com>
2022-03-11 15:23:40 +1000

    Added support for units.

--- R/Molarity.R
+++ R/Molarity.R
@@ -1,11 +1,22 @@
 library(shiny)
 library(tableHTML)
 
-## Define Input UI elements
-calc.input <- function(inputId, integer) {
+## Function to define UI elements.
+calc.input <- function(inputId, integer, units) {
     .placeholdertext <- c('Concentration 1', 'Volume 1', 'Concentration 2', 'Volume 2')[integer]
-    div(style = 'margin: auto; width: 45%; padding: 0px',
-        textInput(inputId = as.character(inputId), placeholder = .placeholdertext, width = '100%', label = '')
+    if (units == 'C') {
+        choices <- list('nM' = 1 ,'μM' = 1000, 'mM' = 1000^2, 'M' = 1000^3)
+    }
+    if (units == 'V') {
+        choices <- list('μL' = 1, 'mL' = 1000, 'L' = 1000^2)
+    }
+    div(style = 'text-align: center',
+        div(style = "margin: auto; display: inline-block;vertical-align:top; width: 60%;",
+            textInput(inputId = paste0(inputId), placeholder = .placeholdertext, width = '100%', label = '')
+        ),
+        div(style = "margin: auto; display: inline-block;vertical-align:top; width: 30%;",
+            selectInput(inputId = paste0('units.', inputId), label = '', choices = choices, selected = choices[2])
+        )
     )
 }
 
@@ -19,13 +30,13 @@ ui <- fluidPage(
             sidebarPanel(
                 tags$style(make_css(list('.well', 'padding', '0px'))),
                 width = 12,
-                calc.input('i1', 1),
-                calc.input('i2', 2),
+                calc.input('i1', 1, 'C'),
+                calc.input('i2', 2, 'V'),
                 div(style = 'text-align: center; font-size: 38px; font-weight: bold; padding: 0px;',
                     "="
                 ),
-                calc.input('i3', 3),
-                calc.input('i4', 4),
+                calc.input('i3', 3, 'C'),
+                calc.input('i4', 4, 'V'),
                 div(style = 'text-align: center; padding: 15px',
                     actionButton('reset', 'Reset')
                 ),
@@ -40,19 +51,23 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     observe({
         inputs <- c(input$i1, input$i2, input$i3, input$i4)
+        unit.inputs <- as.numeric(c(input$units.i1, input$units.i2, input$units.i3, input$units.i4))
+        ## Defines the coefficients to calculate unit differences.
+        conc.ce <- unit.inputs[3] / unit.inputs[1]
+        vol.ce <- unit.inputs[4] / unit.inputs[2]
         if (sum(inputs == '') == 1) {
             missing.int <- which(inputs == '')
             if (missing.int == 1) {
-                solution <- signif(((as.numeric(input$i3) * as.numeric(input$i4)) / as.numeric(input$i2)), digits = 5)
+                solution <- signif((conc.ce * vol.ce * (as.numeric(input$i3) * as.numeric(input$i4)) / as.numeric(input$i2)), digits = 5)
             }
             if (missing.int == 2) {
-                solution <- signif(((as.numeric(input$i3) * as.numeric(input$i4)) / as.numeric(input$i1)), digits = 5)
+                solution <- signif((conc.ce * vol.ce * (as.numeric(input$i3) * as.numeric(input$i4)) / as.numeric(input$i1)), digits = 5)
             }
             if (missing.int == 3) {
-                solution <- signif(((as.numeric(input$i1) * as.numeric(input$i2)) / as.numeric(input$i4)), digits = 5)
+                solution <- signif((conc.ce * vol.ce * (as.numeric(input$i1) * as.numeric(input$i2)) / as.numeric(input$i4)), digits = 5)
             }
             if (missing.int == 4) {
-                solution <- signif(((as.numeric(input$i1) * as.numeric(input$i2)) / as.numeric(input$i3)), digits = 5)
+                solution <- signif((conc.ce * vol.ce * (as.numeric(input$i1) * as.numeric(input$i2)) / as.numeric(input$i3)), digits = 5)
             }
             updateTextInput(
                 inputId = paste0('i', missing.int),
